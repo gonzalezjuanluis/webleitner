40cb0f6e5ef9a2d9f386cf9375d78ce7313a3b8f
gonzalezjuanluis <145705765+gonzalezjuanluis@users.noreply.github.com>
2026-02-10 09:32:27 -0600

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const tarjetas = document.querySelectorAll(".tarjeta");
+let paginaActual = 1;
+let tamañoSi = 22;
 
-    tarjetas.forEach(tarjeta => {
-        tarjeta.addEventListener("click", () => {
-            tarjeta.classList.toggle("volteada");
-        });
-    });
-});
+function siguientePagina() {
+    document.getElementById("p" + paginaActual).classList.remove("activa");
+    paginaActual++;
+    document.getElementById("p" + paginaActual).classList.add("activa");
+}
+
+function rechazar() {
+    tamañoSi += 10;
+    document.getElementById("btnSi").style.fontSize = tamañoSi + "px";
+}
+
+function aceptar() {
+    document.getElementById("p4").classList.remove("activa");
+    document.getElementById("p5").classList.add("activa");
+}
